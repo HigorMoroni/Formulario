f008df5aea51278772dc405c0992fbf120cf1cdd
Higor M. G. Ferreira <moroni.higor@gmail.com>
2020-04-23 20:50:42 -0300

    Validando cpf
    Validador de cpf implementado

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,8 +25,22 @@ class ValidaFormulario {
                 this.criaErro(campo, `Campo ${label} não pode sestar em branco`)
                 valid = false
             }
+
+            if(campo.classList.contains('cpf')) {
+                if(!this.validaCPF(campo)) valid = false
+
+            }
         }
     }
+    validaCPF(campo) {
+        const cpf = new ValidaCPF(campo.value)
+        if (!cpf.valida()) {
+            this.criaErro(campo, 'CPF inválido')
+            return false
+        }
+        return true
+    }
+
     criaErro(campo, msg) {
         const div = document.createElement('div')
         div.innerHTML = msg
